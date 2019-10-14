955e54454794d0217113bc3b80c23247401cf08e
Michiel de Jong <michiel@unhosted.org>
2019-10-14 14:57:53 +0200

    build

---
+++ build/precache-manifest.69646690b1187c227c3f2a37d8653b31.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3409850f661c2af1de7f619c285bbf1c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "73bdf29cdce9fc4fdd82",
+    "url": "/static/css/main.32154f0e.chunk.css"
+  },
+  {
+    "revision": "ef938c762fedd97f97ac",
+    "url": "/static/js/2.4d8d742c.chunk.js"
+  },
+  {
+    "revision": "73bdf29cdce9fc4fdd82",
+    "url": "/static/js/main.7ddbf64d.chunk.js"
+  },
+  {
+    "revision": "30751d67163ef69655b1",
+    "url": "/static/js/runtime-main.0a34a450.js"
+  }
+]);
